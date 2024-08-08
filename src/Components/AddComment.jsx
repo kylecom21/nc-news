126585eef0622d9df0ee4f47f9fa9b32944ef059
Kyle Compton <kylecompton@hotmail.co.uk>
2024-08-08 16:01:33 +0100

    add a comment complete

---
+++ src/Components/AddComment.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { createComment, getCommentByArticleId } from "../../api";
+import { Button } from "@mui/material";
+import { getUsers } from "../../api";
+
+const AddComment = ({ setComments }) => {
+  const { article_id } = useParams();
+  const [username, setUsername] = useState("");
+  const [body, setBody] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((users) => {
+      setUsers(users);
+    });
+  }, []);
+
+  function createHandler(stateFunction) {
+    return function innerHandler(event) {
+      const value = event.target.value;
+      stateFunction(value);
+    };
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    createComment(article_id, username, body)
+      .then(() => {
+        setBody("")
+        setUsername("")
+        return getCommentByArticleId(article_id);
+      })
+      .then((comments) => {
+        setComments(comments);
+      });
+  }
+
+  return (
+    <div>
+      <form className="comment-form">
+        <h1 className="comment-h1">Add a Comment</h1>
+        <label htmlFor="username" className="comment-username">
+          Username:
+          <input
+            list="usernames"
+            onChange={createHandler(setUsername)}
+            type="text"
+            name="usernames"
+            className="comment-input"
+            placeholder="Select a Username"
+            value={username}
+          />
+          <datalist id="usernames">
+            {users.map((user) => (
+              <option value={user.username} key={user.username} />
+            ))}
+          </datalist>
+        </label>
+        <label htmlFor="body" className="comment-comment">
+          Comment:
+          <input
+            onChange={createHandler(setBody)}
+            type="text"
+            name="body"
+            className="comment-input"
+            placeholder="Enter New Comment"
+            value={body}
+          />
+        </label>
+        <Button
+          className="comment-send"
+          variant="contained"
+          sx={{ backgroundColor: "#c9c3b7;", color: "#333" }}
+          onClick={handleSubmit}
+        >
+          Send
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default AddComment;
--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -3,6 +3,7 @@ import { getCommentByArticleId } from "../../api";
 import { useParams } from "react-router-dom";
 import { Button } from "@mui/material";
 import formatDate from "./FormatDate";
+import AddComment from "./AddComment";
 
 const Comments = () => {
   const [comments, setComments] = useState({});
@@ -18,49 +19,43 @@ const Comments = () => {
   return isLoading ? (
     <h2>Loading...</h2>
   ) : (
-    <div className="comments">
-      <h1 className="comment-title">Comments</h1>
-      <input
-        className="comment-input"
-        type="New comment"
-        placeholder="Enter New Comment"
-      />
-      <Button
-        className="comment-send"
-        variant="contained"
-        sx={{ backgroundColor: "#333;", color: "#c9c3b7" }}
-      >
-        Send
-      </Button>
-      {comments.map((comment) => {
-        return (
-          <div className="comment-list" key={comment.comment_id}>
-            <li className="comment-item">
-              <div className="comment-header">
-                <p className="comment-author">{comment.author}</p>
-                <Button
-                  variant="contained"
-                  sx={{ backgroundColor: "#c9c3b7;", color: "#333;" }}
-                >
-                  {" "}
-                  Delete
-                </Button>
-              </div>
-              <p className="comment-body">{comment.body}</p>
-              <div className="comment-footer">
-                <Button
-                  variant="contained"
-                  className="comment-votes"
-                  sx={{ backgroundColor: "#c9c3b7;", color: "#333;" }}
-                >
-                  {comment.votes}
-                </Button>
-                <p className="comment-date">{formatDate(comment.created_at)}</p>
-              </div>
-            </li>
-          </div>
-        );
-      })}
+    <div>
+      <AddComment setComments={setComments} />
+
+      <div className="comments">
+        <h1 className="comment-title">Comments</h1>
+        {comments.map((comment) => {
+          return (
+            <div className="comment-list" key={comment.comment_id}>
+              <li className="comment-item">
+                <div className="comment-header">
+                  <p className="comment-author">{comment.author}</p>
+                  <Button
+                    variant="contained"
+                    sx={{ backgroundColor: "#c9c3b7;", color: "#333;" }}
+                  >
+                    {" "}
+                    Delete
+                  </Button>
+                </div>
+                <p className="comment-body">{comment.body}</p>
+                <div className="comment-footer">
+                  <Button
+                    variant="contained"
+                    className="comment-votes"
+                    sx={{ backgroundColor: "#c9c3b7;", color: "#333;" }}
+                  >
+                    {comment.votes}
+                  </Button>
+                  <p className="comment-date">
+                    {formatDate(comment.created_at)}
+                  </p>
+                </div>
+              </li>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
